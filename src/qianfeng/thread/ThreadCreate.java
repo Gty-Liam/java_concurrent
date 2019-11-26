6bd4196a61fd8d0d9c8fb766b4c9504a09e9d1d4
Liam <244322356@qq.com>
2019-11-26 12:58:46 +0800

    线程创建的方式 实现 Runable 继承 Thread

---
+++ src/qianfeng/thread/ThreadCreate.java
@@ -0,0 +1,55 @@
+package qianfeng.thread;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class ThreadCreate {
+    public static void main(String[] args) {
+        // 自定义类
+        MyThread thread1 = new MyThread();
+
+        // 实现Runable接口1
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                for(int i=0;i<3;i++){
+                    System.out.println("线程" + threadName + " : " + i);
+                }
+                System.out.println("子线程" + threadName + "执行完毕");
+            }
+        };
+        Thread thread2 = new Thread(r1);
+
+        // 外部实现Runable接口2
+        MyRunable r2 = new MyRunable();
+        Thread thread3 = new Thread(r2);
+
+        //start
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        System.out.println("主线程执行完毕");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        String threadName = this.currentThread().getName();
+        for(int i=0; i<3 ; i++){
+            System.out.println("线程" + threadName + " : " + i);
+        }
+        System.out.println("子线程" + threadName + "执行完毕");
+    }
+}
+
+// 实现Runable接口
+class MyRunable implements Runnable {
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        for(int i=0;i<3;i++){
+            System.out.println("线程" + threadName + " : " + i);
+        }
+        System.out.println("子线程" + threadName + "执行完毕");
+    }
+}
