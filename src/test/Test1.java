b8c5cc73583327c33030d0d40e1e291f04dc2e2d
Liam <244322356@qq.com>
2019-12-15 10:49:17 +0800

    delete test

--- src/test/Test1.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public interface Test1{
-    public String getName();
-}
--- src/test/Test2.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Test2 implements Test1 {
-
-    @Override
-    public String getName() {
-        return "Test2";
-    }
-}
